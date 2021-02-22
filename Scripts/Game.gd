bbb3cdd2061eaf64fdd9fd77d4ba55f514fa77b7
Vicky Lee <vicky.winkay.lee@gmail.com>
2021-02-22 20:20:53 +0000

    fix miss scoring

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -119,6 +119,11 @@ func _on_Timer_timeout():
 	_create_hit()
 	$HitStream.position.x -= SONG_VELOCITY;
 	_delete_node()
+	
+	$Combo.text = 'Combo: ' + str(comboStreak)
+	
+	$Score.set_scores(_score)
+	$Score.display_all_scores(_score)
 
 	
 func _has_hit(drumHit):
@@ -139,10 +144,12 @@ func _has_hit(drumHit):
 		$Hit.text = 'OK'
 		_score.good += 1
 		return true
+	_miss()
+
+func _miss():
 	$Hit.text = 'BAD'
 	_score.bad += 1
 	update_combo_streak(0)
-	return false
 
 func update_combo_streak(comboValue):
 	if comboValue == 0:
@@ -166,12 +173,7 @@ func _input(event):
 	if isBlue:
 		_has_hit(Enums.HitType.Blue)
 		print(str(elapsedTime) + ": Enums.HitType.Blue,")
-		
-	$Combo.text = 'Combo: ' + str(comboStreak)
 	
-	$Score.set_scores(_score)
-	$Score.display_all_scores(_score)
-		
 func _create_hit():
 	var key = elapsedTime
 	
@@ -190,3 +192,4 @@ func _delete_node():
 	var node = $HitStream.get_children()[0]
 	if (node.global_position.x <= 200):
 		node.queue_free()
+		_miss()
